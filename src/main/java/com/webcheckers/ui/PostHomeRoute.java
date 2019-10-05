5e04989c30f3d2e6ba89cdbcf18388d8acdf5d77
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-05 18:23:30 -0400

    Stubbed PostHomeRoute.java

---
+++ src/main/java/com/webcheckers/ui/PostHomeRoute.java
@@ -0,0 +1,24 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ * The {@code POST /home} route handler.
+ *
+ * @author Clayton Pruitt
+ * @author Isaias Villaloobos
+ */
+public class PostHomeRoute implements Route {
+
+    public PostHomeRoute() {
+        // TODO
+    }
+
+    @Override
+    public String handle(Request requeset, Response response){
+        // TODO
+        return null;
+    }
+}
