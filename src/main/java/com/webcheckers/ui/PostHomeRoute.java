5a0b4876db26e9510c57f23eb69fc128bcc0101e
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-05 19:58:38 -0400

    Re-implemented PostHomRoute. Created constructor method and stubbed handle method.

---
+++ src/main/java/com/webcheckers/ui/PostHomeRoute.java
@@ -0,0 +1,52 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.PlayerLobby;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+import java.util.Objects;
+
+public class PostHomeRoute implements Route {
+    /**
+     * The PostSignInRoute Class Handling Current Player Input
+     *
+     * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+     * @author <a href='mailto:jrv@se.rit.edu'>Jim Vallino</a>
+     * @contributor Clayton Pruitt : chp4145@rit.edue
+     * @contributor Isaias Villalobos
+     */
+
+    //
+    // Attributes
+    //
+
+    private final PlayerLobby playerLobby;
+    private final TemplateEngine templateEngine;
+
+    //
+    // Constructor
+    //
+
+    public PostHomeRoute(PlayerLobby playerLobby, TemplateEngine templateEngine){
+        // validation
+        Objects.requireNonNull(playerLobby, "playerLobby must not be null, dumdum");
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null, dumdum");
+        //
+        this.playerLobby = playerLobby;
+        this.templateEngine = templateEngine;
+    }
+
+    //
+    // TemplateViewRoute method
+    //
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String handle(Request request, Response response){
+        return null;
+    }
+}
