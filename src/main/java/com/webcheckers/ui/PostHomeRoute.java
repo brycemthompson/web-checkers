cae13295d5700f8a988ce94dcbde39014d467e4c
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-05 18:29:41 -0400

    Implemented constructor for PostHomeRoute.java

--- src/main/java/com/webcheckers/ui/PostHomeRoute.java
+++ src/main/java/com/webcheckers/ui/PostHomeRoute.java
@@ -1,8 +1,12 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.PlayerLobby;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.TemplateEngine;
+
+import java.util.Objects;
 
 /**
  * The {@code POST /home} route handler.
@@ -12,8 +16,31 @@ import spark.Route;
  */
 public class PostHomeRoute implements Route {
 
-    public PostHomeRoute() {
-        // TODO
+    //
+    // Attributes
+    //
+
+    private final PlayerLobby playerLobby;
+    private final TemplateEngine templateEngine;
+
+    /**
+     * The constructor for the {@code POST /home} route handler.
+     *
+     * @param playerLobby
+     *      {@Link PlayerLobby} that holds player data.
+     * @param templateEngine
+     *      template engine to use for rendering HTML page
+     *
+     * @throws NullPointerException
+     *      when the {@code gameCenter} or {@code templateEngine} is null
+     */
+    public PostHomeRoute(PlayerLobby playerLobby, TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(playerLobby, "playerLobby can't be null.");
+        Objects.requireNonNull(templateEngine, "templateEngine can't be null.");
+        //
+        this.playerLobby = playerLobby;
+        this.templateEngine = templateEngine;
     }
 
     @Override
