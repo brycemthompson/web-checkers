f8721bb40e81c42b408b89307eb667b6707ba722
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-08 17:11:20 -0400

    Player object now created for current user. Their username for the sign-out field is now pulled from their Player object.

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -2,6 +2,7 @@ package com.webcheckers.ui;
 
 // THIS CLASS STILL NEED TO BE COMPLETED
 
+import com.webcheckers.Model.Player;
 import com.webcheckers.util.Message;
 import spark.*;
 
@@ -18,6 +19,7 @@ import java.util.logging.Logger;
  * @author <a href='mailto:jrv@se.rit.edu'>Jim Vallino</a>
  * @contributor Bryce Thompson : bxt6698@rit.edu
  * @constributor Daniel Kitchen
+ * @contributor Clayton Pruitt : chp4145@rit.edu
  */
 public class PostSignInRoute implements Route {
 
@@ -26,7 +28,7 @@ public class PostSignInRoute implements Route {
     // Constants
     // Values used in the view-model map for rendering the game view after a guess.
     static final String USERNAME_PARAM = "username";
-    static final String PLAYERSIGNEDIN_PARAM = "currentUser";
+    static final String CURRENTUSER_PARAM = "currentUser";
     private static final Message WELCOME_MSG = Message.info("Sign In to Play!");
     private final TemplateEngine templateEngine;
 
@@ -63,19 +65,6 @@ public class PostSignInRoute implements Route {
      */
     @Override
     public Object handle(Request request, Response response) {
-        /*
-        LOG.finer("GetSignInRoute is invoked.");
-        //
-        Map<String, Object> vm = new HashMap<>();
-        vm.put("title", "Welcome!");
-
-        // display a user message in the Home page
-        vm.put("message", WELCOME_MSG);
-
-        // render the View
-        return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
-         */
-
         LOG.finer("GetSignInRoute is invoked.");
 
         // initialize view-model
@@ -83,12 +72,13 @@ public class PostSignInRoute implements Route {
 
         // get username from template parameters
         final String username = request.queryParams(USERNAME_PARAM);
-        // put the username into our view-model
-        vm.put(PLAYERSIGNEDIN_PARAM, true);
+
+        // put the player into our model
+        vm.put(CURRENTUSER_PARAM, new Player(username));
         vm.put(USERNAME_PARAM, username);
 
         // put the display messages for the home page in the view-model
-        vm.put("title", "Welcome!");
+        vm.put("title", "Welcfome!");
         vm.put("message", WELCOME_MSG);
 
         // render the view
