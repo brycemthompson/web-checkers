213ad050ae2e949c35f1cd988e60801acdbf94ae
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-09 10:40:45 -0400

    Player sign-in authentication updated. Now checks for non-alphanumeric characters. Also displays appropriate failure message on sign-in page.

---
+++ src/main/java/com/webcheckers/Model/Authentication.java
@@ -0,0 +1,11 @@
+package com.webcheckers.Model;
+
+/**
+ * Possible results from player sign-in authentication.
+ * @author Clayton Pruitt : chp4145@rit.edu
+ */
+public enum Authentication {
+    FAIL_INVALID_USERNAME,
+    FAIL_NAME_TAKEN,
+    SUCCESS
+}
