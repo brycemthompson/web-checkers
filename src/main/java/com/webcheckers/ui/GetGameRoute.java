fd7139692c9403d1d8b33026d4df5952c0420368
Isaias Villalobos <ixv8608@rit.edu>
2019-10-09 17:59:22 -0400

    Created two files

---
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -0,0 +1,48 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.util.Message;
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class GetGameRoute implements Route{
+    // Values used in the view-model map for rendering the home view.
+
+    // idk what these are
+    private static final Logger LOG = Logger.getLogger(GetGameRoute.class.getName());
+
+    private static final Message WELCOME_MSG = Message.info("Welcome to the Game Page.");
+
+    private final TemplateEngine templateEngine;
+
+    /**
+     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public GetGameRoute(final TemplateEngine templateEngine) {
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        LOG.config("GetGameRoute is initialized.");
+    }
+
+
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetHomeRoute is invoked.");
+
+        // retrieve the HTTP session
+        final Session session = request.session();
+
+        // start building the view model
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome to Game Page!");
+        vm.put("message", WELCOME_MSG);
+
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "game.ftl"));
+    }
+}
+
---
+++ src/main/java/com/webcheckers/ui/PostGameRoute.java
@@ -0,0 +1,51 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.Model.Player;
+import com.webcheckers.Model.PlayerLobby;
+import com.webcheckers.util.Message;
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class PostGameRoute implements Route{
+    private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+    private static final String WELCOME_MSG = "Game Board";
+
+    // Values used in the view-model map for rendering the game view after a guess.
+    private final TemplateEngine templateEngine;
+
+    PostGameRoute(TemplateEngine templateEngine)
+    {
+        this.templateEngine = templateEngine;
+    }
+
+    /**
+     * Render the WebCheckers Sign In page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Sign In page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetGameRoute is invoked.");
+
+        // initialize view-model
+        final Map<String, Object> vm = new HashMap<>();
+
+        final Session session = request.session();
+
+        // put the display messages for the home page in the view-model
+        vm.put("title", "Game View");
+        vm.put("message", WELCOME_MSG);
+
+        // render the view
+        return templateEngine.render(new ModelAndView(vm, "game.ftl"));
+    }
+}
