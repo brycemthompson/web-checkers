00724b3aa860ba2443e06c07239740a2689466e1
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-16 11:26:25 -0400

    Implemented Board class.

--- src/main/java/com/webcheckers/Model/Board.java
+++ src/main/java/com/webcheckers/Model/Board.java
@@ -1,8 +1,52 @@
 package com.webcheckers.Model;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.Iterator;
 
-    public int row;
-    public int space;
+/**
+ * Java class object representing the board.
+ * @author Clayton Pruitt
+ */
 
+public class Board implements Iterable<Row> {
+
+    // all rows in the board
+    ArrayList<Row> rows;
+
+    // amount of rows in a board
+    int rowsPerBoard = 8;
+
+    /**
+     * Constructor. Automatically populates the board with 8 rows.
+     */
+    public Board(){
+        // populate board with rows
+        rows = new ArrayList<Row>();
+        for (int i = 0; i < rowsPerBoard; i++){
+            rows.add(new Row(i));
+        }
+    }
+
+    @Override
+    public Iterator<Row> iterator() {
+        Iterator<Row> it = new Iterator<Row>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext(){
+                return (currentIndex < rows.size() && rows.get(currentIndex) != null);
+            }
+
+            @Override
+            public Row next(){
+                return rows.get(currentIndex++);
+            }
+
+            @Override
+            public void remove(){
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
 }
