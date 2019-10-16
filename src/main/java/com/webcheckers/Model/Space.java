b6900d5b63ac5e7f9f456f99470dc74b2f2c7155
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-16 11:10:13 -0400

    Updated Space class and updated documentation for Piece class.

--- src/main/java/com/webcheckers/Model/Space.java
+++ src/main/java/com/webcheckers/Model/Space.java
@@ -1,7 +1,45 @@
 package com.webcheckers.Model;
 
-import java.util.ArrayList;
+/**
+ * Java class object representing a space on a board of checkers.
+ * @author Clayton Pruitt
+ * @author Isaias Villalobos
+ */
 
 public class Space {
 
+    // x coordinate of this space, from 0 to 7 (inclusive)
+    private int cellIdx;
+    // y coordinate of this space, from 0 to 7 (inclusive)
+    // inherited from the index of the Row that holds this Space
+    private int cellIdy;
+    // current piece on this space
+    private Piece piece;
+
+    /**
+     * Constructor.
+     */
+    public Space(int cellIdx, int cellIdy, Piece piece){
+        this.cellIdx = cellIdx;
+        this.cellIdy = cellIdy;
+        this.piece = piece;
+    }
+
+    /**
+     * Getter for cellIdX.
+     */
+    public int getCellIdx() {
+        return cellIdx;
+    }
+
+    /**
+     * Getter for piece.
+     */
+    public Piece getPiece(){
+        return this.piece;
+    }
+
+    public boolean isValid(){
+        return ((cellIdx % 2 != cellIdy % 2) && this.piece == null);
+    }
 }
