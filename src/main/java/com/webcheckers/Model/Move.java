e56a614a8a27ff0b3d1e56a72e5cc09e3e36dd28
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-23 10:55:46 -0400

    Updated Position and Move with documentation.

--- src/main/java/com/webcheckers/Model/Move.java
+++ src/main/java/com/webcheckers/Model/Move.java
@@ -2,20 +2,37 @@ package com.webcheckers.Model;
 
 import com.webcheckers.ui.PostHomeRoute;
 
+/**
+ * Represents a Move on the Board by tracking the start Position and the end Position.
+ * @author Clayton Pruitt : chp4145@g.rit.edu
+ */
+
 public class Move {
 
+    // private fields
     private Position start;
     private Position end;
 
+    /**
+     * Constructor.
+     * @param start the Position this Move started in
+     * @param end the Position this Move is trying to go to
+     */
     public Move(Position start, Position end){
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Getter for start.
+     */
     public Position getStart(){
         return this.start;
     }
 
+    /**
+     * Getter for end.
+     */
     public Position getEnd(){
         return this.end;
     }
--- src/main/java/com/webcheckers/Model/Position.java
+++ src/main/java/com/webcheckers/Model/Position.java
@@ -1,19 +1,37 @@
 package com.webcheckers.Model;
 
+/**
+ * Represents the (row, cell) coordinate of some Space on the Board.
+ * @author Clayton Pruitt : chp4145@g.rit.edu
+ */
+
 public class Position {
 
+    // private fields
     private int row;
     private int cell;
 
+
+    /**
+     * Constructor.
+     * @param row the row this Position is located in
+     * @param cell the cell this Position is located in
+     */
     public Position(int row, int cell){
         this.row = row;
         this.cell = cell;
     }
 
+    /**
+     * Getter for row.
+     */
     public int getRow(){
         return this.row;
     }
 
+    /**
+     * Getter for cell.
+     */
     public int getCell(){
         return this.cell;
     }
