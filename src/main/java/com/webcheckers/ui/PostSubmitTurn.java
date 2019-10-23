add3f922b534ccb5e25556375cdbe4aa876b2251
Clayton Pruitt <clay10pruitt@gmail.com>
2019-10-23 10:48:58 -0400

    Created stubbed classes for PostBackupMove, PostSubmitTurn, and PostValidateMove. All still need a constructor and none have been initialized in WebServer yet.

---
+++ src/main/java/com/webcheckers/ui/PostSubmitTurn.java
@@ -0,0 +1,13 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PostSubmitTurn implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
