ee7c28c4af927edcceaba9757eb35805c36bfa28
Bryce Thompson <brycet11@yahoo.com>
2019-11-02 20:30:46 -0400

    Populated the GetSignInRouteTest

---
+++ src/test/java/com/webcheckers/ui/GetSignInRouteTest.java
@@ -0,0 +1,68 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.Model.PlayerLobby;
+import com.webcheckers.util.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GetSignInRouteTest
+{
+    private TemplateEngineTester templateEngineTester = new TemplateEngineTester();
+    private TemplateEngine templateEngine;
+    private PlayerLobby playerLobby;
+    private Request request;
+    private GetSignInRoute CuT;
+    private Response response;
+    private Session session;
+
+    @BeforeEach
+    public void setup(){
+
+        request = mock(Request.class);
+        response = mock(Response.class);
+        templateEngine  = mock(TemplateEngine.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+
+        playerLobby = new PlayerLobby();
+        CuT = new GetSignInRoute(templateEngine, playerLobby);
+        templateEngineTester = new TemplateEngineTester();
+    }
+
+    @Test
+    public void test_signin_title_and_msg(){
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(templateEngineTester.makeAnswer());
+
+        CuT.handle(request, response);
+        templateEngineTester.assertViewModelAttribute("title", "Sign in to play!");
+    }
+
+    @Test
+    public void test_message_display(){
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(templateEngineTester.makeAnswer());
+
+        Message msg = new Message("Test1", Message.Type.INFO);
+        when(session.attribute(ConstsUI.MESSAGE_PARAM)).thenReturn(msg);
+
+        //session.attribute(ConstsUI.MESSAGE_PARAM, msg );
+
+        CuT.handle(request, response);
+        templateEngineTester.assertViewModelAttribute(ConstsUI.MESSAGE_PARAM, msg);
+    }
+
+    @Test
+    public void test_welcome_message(){
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(templateEngineTester.makeAnswer());
+
+        CuT.handle(request, response);
+        templateEngineTester.assertViewModelAttribute(ConstsUI.MESSAGE_PARAM, ConstsUI.WELCOME_MSG);
+
+
+    }
+}
