b3d4462e954be52cbe7b753cd8f5fa64779d699b
Isaias Villalobos <ixv8608@rit.edu>
2019-11-20 23:00:21 -0500

    PieceWithPosition is now complete

---
+++ src/test/java/model/PieceWithPositionTest.java
@@ -0,0 +1,60 @@
+package model;
+import com.webcheckers.Model.Piece;
+import com.webcheckers.Model.PieceWithPosition;
+import com.webcheckers.Model.Position;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("Model")
+public class PieceWithPositionTest {
+   @Test
+    public void get_piece_tes(){
+        Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
+        PieceWithPosition piece = new PieceWithPosition(p, new Position(2,3));
+        assertTrue(piece instanceof PieceWithPosition);
+        assertEquals(p, piece.getPiece());
+    }
+
+    @Test
+    public void get_piece_position(){
+        Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
+        Position pos =  new Position(2,3);
+        PieceWithPosition piece = new PieceWithPosition(p, pos);
+        assertEquals(pos, piece.getPosition());
+    }
+
+    @Test
+    public void get_row_test(){
+        Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
+        Position pos =  new Position(2,3);
+
+        PieceWithPosition piece = new PieceWithPosition(p, pos);
+
+        assertEquals(pos.getRow(), piece.getRow());
+    }
+
+    @Test
+    public void get_cell_test(){
+        Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
+        Position pos =  new Position(2,3);
+
+        PieceWithPosition piece = new PieceWithPosition(p, pos);
+
+        assertEquals(pos.getCell(), piece.getCell());
+    }
+
+    @Test
+    public void equal_test(){
+        Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
+        Position pos =  new Position(2,3);
+
+        PieceWithPosition piece = new PieceWithPosition(p, pos);
+        PieceWithPosition newPiece= new PieceWithPosition(p, pos);
+
+        assertTrue(piece.equals(newPiece));
+    }
+
+
+}
