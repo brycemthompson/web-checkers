681e78afb52c81c812d86df0c045208379377064
Isaias Villalobos <ixv8608@rit.edu>
2019-11-20 23:08:08 -0500

    Added documentation, and all tests pass

--- src/test/java/model/PieceWithPositionTest.java
+++ src/test/java/model/PieceWithPositionTest.java
@@ -7,8 +7,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ This class is going to test whether the function created in the original code was properly
+ created and returns the correct values
+ */
 @Tag("Model")
 public class PieceWithPositionTest {
+
+    /**
+        This test is going to test whether or not the piece object
+        created matches the object that gets returned
+        when the getPiece function is called from the original code.
+     */
    @Test
     public void get_piece_tes(){
         Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
@@ -17,6 +27,10 @@ public class PieceWithPositionTest {
         assertEquals(p, piece.getPiece());
     }
 
+    /**
+     This test is going to test against the original code. It will use a sample created object such as
+     pos, which will be compared to a PieceWithPositionn's getPosition Piece object.
+     */
     @Test
     public void get_piece_position(){
         Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
@@ -24,7 +38,10 @@ public class PieceWithPositionTest {
         PieceWithPosition piece = new PieceWithPosition(p, pos);
         assertEquals(pos, piece.getPosition());
     }
-
+    /**
+     This test test the original code's getRow function. A sample position object was created and
+     it being tested against the a PieceWithPosition Object's getRow function
+     */
     @Test
     public void get_row_test(){
         Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
@@ -34,7 +51,10 @@ public class PieceWithPositionTest {
 
         assertEquals(pos.getRow(), piece.getRow());
     }
-
+    /**
+     This test test the original code's getCell function. A sample position object was created and
+     it being tested against the a PieceWithPosition Object's getCell function
+     */
     @Test
     public void get_cell_test(){
         Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
@@ -44,7 +64,10 @@ public class PieceWithPositionTest {
 
         assertEquals(pos.getCell(), piece.getCell());
     }
-
+    /**
+     This test is simply testing whether or not the two objects, in this case,
+     two PieceWithPosition objects are the same or not
+     */
     @Test
     public void equal_test(){
         Piece p  = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
